43b60bbb19ad9ebcf95020370de0547116901a61
Anna Budnikowska <aniax@poczta.onet.pl>
2026-03-04 15:28:14 +0100

    removing comments

--- tests/uniqueness-of-name.spec.ts
+++ tests/uniqueness-of-name.spec.ts
@@ -3,11 +3,6 @@ import { test } from '../page-object-model/fixtures/howling-test.fixture';
 test('Protecting the uniqueness of the name', async ({ homePage }) => {
   const character1 = {name: 'Bohater', role: 'Warrior',  powers: ['20', '15', '10', '10'] }
   const character2 = {name: 'bohater', role: 'Warrior',  powers: ['15', '15', '10', '15'] }
-  // const name1 = 'Bohater';
-  // const name2 = 'bohater';
-  // const role = 'Warrior';
-  // const powers1 = ['20', '15', '10', '10'];
-  // const powers2 = ['15', '15', '10', '15']
 
   await homePage.open();
   await homePage.clickAcceptButton();
@@ -19,6 +14,6 @@ test('Protecting the uniqueness of the name', async ({ homePage }) => {
   await homePage.chooseRole(character2.role);
   await homePage.fillPowers(character2.powers);
   await homePage.addCharacter();
-  await homePage.chceckPopup();
+  await homePage.checkPopup();
   await homePage.closePage();
 });
